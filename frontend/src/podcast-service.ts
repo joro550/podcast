cc171a587e60296ce86df6fdf884084c51e2b019
markdavies <markdavies550@gmail.com>
2024-10-05 08:08:14 +0100

    more testing

--- frontend/src/podcast-service.ts
+++ frontend/src/podcast-service.ts
@@ -6,6 +6,7 @@ export default class PodcastService {
   constructor() {
     this._baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL ?? "";
     console.log("Getting backend url", process.env.NEXT_PUBLIC_BACKEND_URL);
+    console.log("ENV", process.env);
   }
 
   public async getWeather(): Promise<Weather[]> {
